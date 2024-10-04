468a8cbdf7046d3f980fae06825b959ba4fd2b55
Ferap1522 <75100007@institutocajas.info>
2024-10-04 11:58:57 -0500

    Add files via upload

---
+++ semana05/ejercicio3.dart
@@ -0,0 +1,12 @@
+BigInt factorial(int num) {
+  BigInt resultado = BigInt.one;
+  for (int i = 1; i <= num; i++) {
+    resultado *= BigInt.from(i);
+  }
+  return resultado;
+}
+
+void main() {
+  int num = 100; 
+  print('El factorial de $num es: ${factorial(num)}');
+}
